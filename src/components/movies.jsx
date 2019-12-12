4b22759b87047b7a01976d63dedbeadf71718846
randolphjr7 <randolphjr7@gmail.com>
2019-12-12 18:27:12 -0500

    initial commit

---
+++ src/components/movies.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    console.log(movie);
+    const newMoviesList = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies: newMoviesList });
+  };
+
+  getMovieListLength() {
+    console.log(this.state.movies.length);
+    if(this.state.movies.length === 0){
+      return <h1>There are no movies in the database</h1>;
+    }
+    else if(this.state.movies.length === 1){
+      return <h1>Showing {this.state.movies.length} movie in the database</h1>
+    }
+    return <h1>Showing {this.state.movies.length} movies in the database.</h1>;
+  }
+
+
+  render() {
+    return (
+      
+      <div>
+
+      {this.getMovieListLength()}
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(eachMovie => (
+              <tr key={eachMovie._id}>
+                <td>{eachMovie.title}</td>
+                <td>{eachMovie.genre.name}</td>
+                <td>{eachMovie.numberInStock}</td>
+                <td>{eachMovie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(eachMovie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </div>
+    );
+  }
+}
+
+export default Movies;
